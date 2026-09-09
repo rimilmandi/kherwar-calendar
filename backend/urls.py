930dd66d64977026e4a16148f91078a8655f445e
Rimil Mandi <rimilmandi@gmail.com>
2026-09-09 22:10:14 +0530

    Fixed urls and settings for production

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth import views as auth_views
 from store import views
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
     path('admin/', admin.site.urls),  # উভয় admin URL রাখতে পারেন
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
